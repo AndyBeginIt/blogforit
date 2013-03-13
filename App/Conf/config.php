8d71dfb1d6f6ea502de157d29fbbb9321d131a9e
AndyBeginIt <zhexiao@163.com>
2013-03-12 22:29:16 -0400

    Revert "Revert "add url route .""
    This reverts commit 990632386fa87930ef5511f087600d54de69ab5d.

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -6,7 +6,7 @@ $arr2 = require 'email.inc.php';
 $arr3 = array(
     //'配置项'=>'配置值'
     'SHOW_PAGE_TRACE'=>true,
-    //'URL_HTML_SUFFIX'=>'.html',
+    'URL_HTML_SUFFIX'=>'.html',
 	
 
 	'APP_GROUP_LIST' => 'Home,Admin',
@@ -28,6 +28,19 @@ $arr3 = array(
     'DEFAULT_LANG' => 'en-us', 		// 默认语言
     'LANG_AUTO_DETECT' => true, 	// 自动侦测语言
     'LANG_LIST'=>'en-us,zh-cn',		//必须写可允许的语言列表
+
+    //Url 
+    'URL_MODEL'	=>	2,
+
+    //URL路由,重定义每个路径的url
+    'URL_ROUTER_ON'   => true, //开启路由
+	'URL_ROUTE_RULES' => array( 
+		//定义路由规则
+	    'blog/:b_id'	 =>		'View/index',	
+	    'category/:c_id' =>		'Index/index',
+	  	'tag/:tags'		=>		'Index/index',
+	  	'archive/:date'	=>		'Index/index',
+	),
 	
 );
 
